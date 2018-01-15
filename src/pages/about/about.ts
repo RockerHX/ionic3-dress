b22c4b4aa467e8c0248a57fe34f633d5612958e9
RockerHX <rockerhx@gmail.com>
2018-01-15 16:48:39 +0800

    Update[Page]: About

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ThemeableBrowser } from '@ionic-native/themeable-browser';
 
 @IonicPage()
 @Component({
@@ -8,8 +9,41 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    { title: "我的淘宝", link: "https://h5.m.taobao.com/mlapp/mytaobao.html#mlapp-mytaobao" },
+    { title: "购物车", link: "https://h5.m.taobao.com/mlapp/cart.html" },
+    { title: "我的订单", link: "https://h5.m.taobao.com/mlapp/olist.html" },
+    { title: "待付款", link: "https://h5.m.taobao.com/mlapp/olist.html?spm=a2141.7756461.2.1&tabCode=waitPay" },
+    { title: "待发货", link: "https://h5.m.taobao.com/mlapp/olist.html?spm=a2141.7756461.2.2&tabCode=waitSend" },
+    { title: "待收货", link: "https://h5.m.taobao.com/mlapp/olist.html?spm=a2141.7756461.2.3&tabCode=waitConfirm" },
+    { title: "待评价", link: "https://h5.m.taobao.com/mlapp/olist.html?spm=a2141.7756461.2.4&tabCode=waitRate" }
+  ];
+  
+  constructor(public navCtrl: NavController, private themeableBrowser: ThemeableBrowser) {
+  }
 
+  itemClick(item) {
+    let options = {
+      statusbar: {
+        color: '#f8285c'
+      },
+      toolbar: {
+        height: 44,
+        color: '#f8285c'
+      },
+      title: {
+        color: '#ffffffff',
+        showPageTitle: true
+      },
+      backButton: {
+        image: 'back',
+        imagePressed: 'back_pressed',
+        align: 'left',
+        event: 'backPressed'
+      },
+      backButtonCanClose: true
+    };
+    this.themeableBrowser.create(item.link, '_blank', options);
   }
 
 }
