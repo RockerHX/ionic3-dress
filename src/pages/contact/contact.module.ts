c30dd1bf82f1c591f473e87633e6052ab7d72835
RockerHX <rockerhx@gmail.com>
2018-01-15 16:36:50 +0800

    Update[Page]: Contact

--- src/pages/contact/contact.module.ts
+++ src/pages/contact/contact.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ContactPage } from './contact';
 
+import { ComponentsModule } from '../../components/components.module';
+
 @NgModule({
     declarations: [
         ContactPage,
     ],
     imports: [
         IonicPageModule.forChild(ContactPage),
+        ComponentsModule
     ],
 })
 export class ContactPageModule { }
